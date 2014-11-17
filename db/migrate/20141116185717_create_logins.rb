cc9f2c7caf691ab64d2ccdbcf475e21ca3d9d1d6
GurvirCheema <GurvirCheema@hotmail.com>
2014-11-17 12:29:29 -0800

    Email added to login, removed from site

--- db/migrate/20141116185717_create_logins.rb
+++ db/migrate/20141116185717_create_logins.rb
@@ -2,6 +2,7 @@ class CreateLogins < ActiveRecord::Migration
   def change
     create_table :logins do |t|
       t.integer :site_id
+      t.string :email
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116210114) do
+ActiveRecord::Schema.define(version: 20141116205951) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141116210114) do
 
   create_table "logins", force: true do |t|
     t.integer  "site_id"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password",  default: "", null: false
@@ -68,7 +69,6 @@ ActiveRecord::Schema.define(version: 20141116210114) do
     t.integer  "manager_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email"
   end
 
   create_table "workers", force: true do |t|
