e5b07f0cf5f826e42059ad3db0eddecfca232de1
GurvirCheema <GurvirCheema@hotmail.com>
2014-11-19 10:59:04 +1100

    Add helpers methods to check for the user rights

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :superuser?, :manager?, :manager
+
+  protected
+
+  def superuser?
+    current_login && current_login.superuser?
+  end
+
+  def manager?
+    current_login && current_login.loggable.present?
+  end
+
+  def manager
+    manager? ? current_login.loggable : nil
+  end
+
+  def require_superuser
+    redirect_to root_path, alert: "You are not allowed to perform this operation" unless superuser?
+  end
+
+  def require_manager
+    redirect_to root_path, alert: "You are not allowed to perform this operation" unless (manager? || superuser?)
+  end
 end
