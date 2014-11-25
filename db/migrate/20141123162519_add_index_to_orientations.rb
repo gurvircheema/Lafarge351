6313f7810363e57eb8b59402cdf6ffbd21c10d38
GurvirCheema <GurvirCheema@hotmail.com>
2014-11-24 22:50:23 -0800

    Remove uniqueness from worker_id from orientations

--- db/migrate/20141123162519_add_index_to_orientations.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToOrientations < ActiveRecord::Migration
-  def change
-    add_index :orientations, [:site_id, :worker_id], :unique => true, :name => 'one orientation of worker per site'
-  end
-end
